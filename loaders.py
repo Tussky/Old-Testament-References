b2ec78a6535ff61d5762ddd55b86a50cb0679b5b
Wesley Stevick <12481632w@gmail.com>
2025-11-04 16:52:40 -0500

    old testament book use by new testament book

--- loaders.py
+++ loaders.py
@@ -28,14 +28,18 @@ def load_strong():
 def load_references_naive():
     return loader("references-naive")
 
+
 def load_munged_references():
     return loader("references2")
+
 
 def load_bible():
     return loader("bible")
 
+
 def load_strongs():
     return loader("strongs")
+
 
 def load_rmac():
     return loader("rmac_df")
